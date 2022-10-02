34abada43c4a50cc6b62e4350b69a47507be3015
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-10-02 17:58:01 +0200

    Remove analyze modes(will be replaced to lsp models)

--- backend/api/common_api/src/dto/code/mod.rs
+++ backend/api/common_api/src/dto/code/mod.rs
@@ -1,3 +1,2 @@
-pub mod analyze_result_dto;
 pub mod file_dto;
 pub mod raw_message_dto;
--- backend/feature/code/code_domain/src/model/analyze_result.rs
+++
@@ -1,7 +0,0 @@
-use super::lint::Lint;
-
-#[derive(PartialEq, Eq, Debug)]
-pub struct AnalyzeResult {
-    pub success: bool,
-    pub lints: Vec<Lint>,
-}
